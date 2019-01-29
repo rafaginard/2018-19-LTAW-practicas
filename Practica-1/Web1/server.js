38842a38be04e1ab96b5254545c481d477a0b9f0
Rafael Ginard Aguilera <rginard@beta04.aulas.gsyc.urjc.es>
2019-01-29 12:42:57 +0100

    Mi primerito index

--- Practica-1/Web1/server.js
+++ Practica-1/Web1/server.js
@@ -1,10 +1,12 @@
 var http = require('http');
-
+var fs = require('fs');
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Peticion atendida")
+  res.end(data);
+  console.log("Peticion Atendida")
+});
 }).listen(8080);
