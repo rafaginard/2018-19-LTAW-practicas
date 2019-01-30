c6c9789ef68427314ad4560d8675a1ce8c580463
Rafael Ginard Aguilera <rginard@alpha30.aulas.gsyc.urjc.es>
2019-01-30 13:58:20 +0100

    Funciona segun el documento que pidas

--- Practica-1/Web1/server.js
+++ Practica-1/Web1/server.js
@@ -1,12 +1,23 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data){
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+
+  fs.readFile(filename, function(err, data){
+    //Control por si el server no funciona.
+    if (err){
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Fount");
+    }
+
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(data);
+  res.write(data);
+  res.end();
   console.log("Peticion Atendida")
 });
 }).listen(8080);
