ff4954fb3abc0ac7c2abfa424a5c012b83628e36
Rafael Ginard Aguilera <rginard@beta04.aulas.gsyc.urjc.es>
2019-02-11 12:56:00 +0100

    Comprobar json en servidor

--- Practica-1/Web1/server.js
+++ Practica-1/Web1/server.js
@@ -2,12 +2,14 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 console.log("Arrancando servidor...")
-
-
+//var fs=require('fs');
+//var data=fs.readFileSync('products.json', 'utf8');
+//var words=JSON.parse(data);
+//MIRAR SI FUNCIONA POR QUE ESTO ES UNA MIERDA....
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
-  
+
   if (q.pathname == ('/')){
     filename = "index.html";
   }
